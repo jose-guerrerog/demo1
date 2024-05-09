06dfcaf4edf1eda6dfb4ecca3d5460883e4aa507
jose-guerrerog <joseguerrerog880@gmail.com>
2024-05-09 12:17:17 +1000

    Updates

---
+++ component---src-pages-404-js-388c640b3250dccdec6d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[125],{8899:function(t,e,n){n.r(e),n.d(e,{Head:function(){return s}});var a=n(6540),u=n(4832),l=n(8296);const s=()=>a.createElement(l.A,{title:"404: Not Found"});e.default=()=>a.createElement(u.A,null,a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-388c640b3250dccdec6d.js.map
