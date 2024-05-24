9ff898d972af43498a26b6ffc0829230de7e26f3
jose-guerrerog <joseguerrerog880@gmail.com>
2024-05-24 11:49:18 +1000

    adding image

--- src/components/header.js
+++ src/components/header.js
@@ -6,78 +6,100 @@ import IconButton from "@mui/material/IconButton"
 import MenuIcon from "@mui/icons-material/Menu"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
-import {
-  Menu,
-  MenuItem,
-} from "@mui/material";
+import { Menu, MenuItem } from "@mui/material"
+import { motion } from 'framer-motion'
 
-
-const Header = ({ siteTitle, menuLinks}) => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
+const Header = ({ siteTitle, menuLinks }) => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null)
+  const handleOpenNavMenu = event => {
+    setAnchorElNav(event.currentTarget)
+  }
 
   const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    setAnchorElNav(null)
+  }
 
   return (
-  <AppBar position="sticky">
-    <Toolbar>
-      <IconButton
-        color="inherit"
-        aria-controls="menu-appbar"
-       aria-haspopup="true"
-        edge="start"
-        onClick={handleOpenNavMenu}
-        sx={{ mr: 2, display: { sm: "none" } }}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Typography
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+          edge="start"
+          onClick={handleOpenNavMenu}
+          sx={{ mr: 2, display: { sm: "none" } }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography
+          component={motion.h2}
+          exit={{ y: -1000 }}
+          variant="h6"
+          sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+        >
+          Eastside Learning Centre
+        </Typography>
+
+        {/* <Typography
         variant="h6"
         component="div"
         sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
       >
         Eastside Learning Centre
-      </Typography>
-      <Box sx={{ display: { xs: "none", sm: "block" } }}>
-        {menuLinks.map(item => (
-          <Link to={item.link} key={item.label} color="#fff" style={{color: "#fff", marginRight: '20px', textDecoration: 'none'}}>
-          {item.name}
-        </Link>
-        ))}
-      </Box>
-      <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorElNav}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top", // Adjust to match the new position
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorElNav)}
-                  onClose={handleCloseNavMenu}
-                  sx={{
-                    display: { xs: "block", md: "none" },
-                  }}
-                >
-                   {menuLinks.map(item => (
-                      <MenuItem>
-                        <Link to={item.link} style={{ color: "#000", marginRight: '20px', textDecoration: 'none'}}>
-                        {item.name}
-                      </Link>
-                      </MenuItem>
-                    
-                    ))}
-                </Menu>
-    </Toolbar>
-  </AppBar>
-)}
+      </Typography> */}
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          {menuLinks.map(item => (
+            <Link
+              to={item.link}
+              key={item.label}
+              color="#fff"
+              style={{
+                color: "#fff",
+                marginRight: "20px",
+                textDecoration: "none",
+              }}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </Box>
+        <Menu
+          id="menu-appbar"
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "right",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top", // Adjust to match the new position
+            horizontal: "right",
+          }}
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{
+            display: { xs: "block", md: "none" },
+          }}
+        >
+          {menuLinks.map(item => (
+            <MenuItem>
+              <Link
+                to={item.link}
+                style={{
+                  color: "#000",
+                  marginRight: "20px",
+                  textDecoration: "none",
+                }}
+              >
+                {item.name}
+              </Link>
+            </MenuItem>
+          ))}
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  )
+}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,42 +2,105 @@ import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Box, Button, Container, Typography } from '@mui/material'
-import CheckIcon from '@mui/icons-material/Check';
+import { Box, Button, Container, Typography } from "@mui/material"
+import { StaticImage } from "gatsby-plugin-image"
+import CheckIcon from "@mui/icons-material/Check"
+import { motion } from 'framer-motion'
 
 const features = [
-  'Start your child on the road to success.',
-  'Turn Non-Readers into Readers!',
+  "Start your child on the road to success.",
+  "Turn Non-Readers into Readers!",
   "Unlock your child's full potential by building their self esteem and confidence.",
-  'Support your children, they do not need to fail.',
-  'Provide your child with early intervention and support, using explicit teaching methods and individual programs that will improve their literacy and numeracy skills.',
-  'Supply you with regular progress reports.',
-  'Design individual ﬂexible programs based on diagnostic assessments of your child',
-];
+  "Support your children, they do not need to fail.",
+  "Provide your child with early intervention and support, using explicit teaching methods and individual programs that will improve their literacy and numeracy skills.",
+  "Supply you with regular progress reports.",
+  "Design individual ﬂexible programs based on diagnostic assessments of your child",
+]
+
+const variantImg = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: 1,
+      when: "beforeChildren",
+    },
+  },
+}
 
 const IndexPage = () => (
-<Layout>
-  <SEO title="Home" />
-  <Typography variant="h3" fontWeight={700}>Welcome to Eastside Learning Centre</Typography>
-  <Typography variant="body1" mt={5} textAlign="justify" sx={{maxWidth: '100%'}}>Eastside Learning Centre is situated in the Eastern Suburbs servicing the areas of Matraville, Maroubra, Malabar, Mascot Coogee, Kensington, Kingsford, Chifley, Botany, Rosebery, Daceyville, Pagewood, Eastgardens, Randwick and Little Bay.</Typography>
-  <Typography variant="body1" mt={2}>Eastside Learning Centre is here to:</Typography>
+  <Layout>
+    <SEO title="Home" />
+    <Typography variant="h3" fontWeight={700}>
+      Welcome to Eastside Learning Centre
+    </Typography>
+    <Typography
+      variant="body1"
+      mt={5}
+      textAlign="justify"
+      sx={{ maxWidth: "100%" }}
+    >
+      Eastside Learning Centre is situated in the Eastern Suburbs servicing the
+      areas of Matraville, Maroubra, Malabar, Mascot Coogee, Kensington,
+      Kingsford, Chifley, Botany, Rosebery, Daceyville, Pagewood, Eastgardens,
+      Randwick and Little Bay.
+    </Typography>
+    <Box my={4} sx={{display: 'flex', justifyContent: 'center'}}>
+    <StaticImage
+          src="../images/eastside-image.jpeg"
+          placeholder="blurred"
+          width={500}
+          height={500}
+          formats={["auto", "webp", "avif"]}
+          alt="A Dog Image"
+          transformOptions={{ fit: "cover", cropFocus: "attention" }}
+        />
+    </Box>
+    
+    <Typography variant="body1" mt={2}>
+      Eastside Learning Centre is here to:
+    </Typography>
 
-  <Box sx={{ display: 'flex', flexDirection: 'column'}} my={5}>
-    {
-      features.map((feature) => (
-        <Box sx={{display: 'flex', alignItems: 'flex-start', flexDirection: 'row'}} mb={2}>
+    <Box sx={{ display: "flex", flexDirection: "column" }} my={5}>
+      {features.map(feature => (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "flex-start",
+            flexDirection: "row",
+          }}
+          mb={2}
+        >
           <CheckIcon />
-          <Typography variant="body1" sx={{maxWidth: '100%'}} ml={2}>{feature}</Typography>
+          <Typography variant="body1" sx={{ maxWidth: "100%" }} ml={2}>
+            {feature}
+          </Typography>
         </Box>
-      ))
-    }
-  </Box>
+      ))}
+    </Box>
 
-  <Typography variant="body1" textAlign="justify" sx={{maxWidth: '100%'}} mb={2}>Our individual programs are curriculum based and taught by teachers and university specialists qualified in their field.</Typography>
+    <Typography
+      variant="body1"
+      textAlign="justify"
+      sx={{ maxWidth: "100%" }}
+      mb={2}
+    >
+      Our individual programs are curriculum based and taught by teachers and
+      university specialists qualified in their field.
+    </Typography>
 
-  <Typography variant="body1" textAlign="justify" sx={{maxWidth: '100%'}}>We provide One to One tuition for students in literacy and numeracy, from pre school through to year 12, in a caring, safe and nurturing environment, with up to date resources.</Typography>
+    <Typography mb={4} variant="body1" textAlign="justify" sx={{ maxWidth: "100%" }}>
+      We provide One to One tuition for students in literacy and numeracy, from
+      pre school through to year 12, in a caring, safe and nurturing
+      environment, with up to date resources.
+    </Typography>
 
-</Layout>
+  </Layout>
 )
 
 export default IndexPage
